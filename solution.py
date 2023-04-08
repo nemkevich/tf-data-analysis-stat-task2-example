00b3488278831ff9253d0844d3cdb187977b758d
Maxim Nemkevich <nemkevichma@gmail.com>
2023-04-08 03:29:00 +0200

    3 retake

--- solution.py
+++ solution.py
@@ -8,14 +8,15 @@ from scipy.stats import chi2
 chat_id = 82953459 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    
     alpha = 1 - p
-    stat = np.square(x).sum()
+    n = len(x)
     
-    left_bound = np.sqrt(stat / (62 * chi2.ppf(1 - alpha / 2, 2 * len(x))))
-    right_bound = np.sqrt(stat / (62 * chi2.ppf(alpha / 2, 2 * len(x))))
-            
-    return left_bound, right_bound
+    x2 = np.array([xi**2 for xi in x])
+    x2_mean = x2.mean()
+    
+    chi2_rv = chi2(df = 2 * n)
+    
+    left = chi2_rv.ppf(1 - alpha / 2) #b
+    right = chi2_rv.ppf(alpha / 2) #a
+    
+    return np.sqrt(n * x2_mean / (left * 62)), np.sqrt(n * x2_mean / (right * 62))
